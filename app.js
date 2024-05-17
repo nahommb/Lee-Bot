1892b8e39956af9fb3f8224f64cadcecc5f8108e
Nahom Melese <nahomjr17@gmail.com>
2024-05-17 09:55:17 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const ex = require('express')
+const telegram = require('node-telegram-bot-api')
+const bot = new telegram('7102544602:AAGigJfNSBb_9yRnvxsH1_QDC8lhcJRtMjM',{polling:true})
+const app = ex()
+
+bot.on('message',(msg)=>{
+    const chatId = msg.chat.id;
+    const messageText = msg.text;
+
+    console.log(msg);
+    // Respond to the user
+    // bot.sendMessage(chatId, `You said: ${messageText}`);
+    
+    bot.sendMessage(chatId,`Hello ${msg.chat.first_name} አርሰናል ሻምፒዮን አይሆንም::
+    የአርሰናል ደጋፊ ከሆንክ ይሄን አስታውስ ዋንጫ አታቅም ወደፊትም ዘለአለምህን ዋንጫ አታይም ምክንያቱም ይሄ አርሰናል ነው ትናንትም ዛሬም ነገም ከነገወዲያም ትንሽ ከለብ ናቹ ጨርሻለው ደግመህ አንብበው 🦶🦶🦶🦶🦶🦶🦶🦶🦶🦶🦶`)
+})
+
+// bot.sendPhoto(873484934,'AgACAgQAAxkBAAM8ZkS1hhcZnoYxz-7nP6iwfAabv9UAAjHJMRsgRyBS5m3huSitnvEBAAMCAANtAAM1BA')
+app.listen('3000',function(){
+    console.log('running at port 3000')
+})
